453501ed864b01280b713581095476d1d14a20e3
Welton Felix <52381662+weltonfelix@users.noreply.github.com>
2019-12-24 12:54:25 -0300

    add new features: login and back to login screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+const div = document.querySelector('div#main')
 function hs(){
+	const senha = document.getElementById('pass')
     const sh = document.getElementById('sh')
-    const senha = document.getElementById('pass')
     if(senha.type == 'password'){
         senha.type = 'show'
         sh.value = '🐵'
@@ -8,4 +9,19 @@ function hs(){
         senha.type = 'password'
         sh.value = '🙈'
     }
+}
+function logar(){
+	const senha = document.getElementById('pass')
+	const mail = document.getElementById('mail')
+	if (senha.value.length == 0 || mail.value.length == 0){
+		alert('Preencha todos os campos!')
+	}else{
+		div.innerHTML = "<h3 id='works'>Login realizado com sucesso!</h3>"
+		div.innerHTML += "<p><input type='button' id='back' onclick='back()' value='Voltar'></p>"
+	}
+}
+function back(){
+	div.innerHTML = `<p>E-mail: <input type="email" name="e-mail" id="mail"></p>
+	<p>Senha: <input type="password" name="senha" id="pass"><input type="button" onclick="hs()" id="sh" value="🙈"></p>
+	<p><input type='button' id='next' onclick='logar()' value='Próximo'></p>`;
 }
